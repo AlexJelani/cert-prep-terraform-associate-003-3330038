7073cc77ad82b5da4ba8b15cc8c9bae461f8ebbe
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2024-08-17 11:04:18 +0900

    import existing infra into terraform state

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
-resource "aws_instance" "MyInstance_test" {
+resource "aws_instance" "ldap-ubuntu" {
     #instance configuration
+  ami = "ami-0123456789abcdef0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ldap-ubuntu"
+  }
+
 }
+#terraform import aws_instance.MyInstance_test i-0123456789abcdef0
+#terraform import aws_instance.ldap-ubuntu i-0123456789abcdef0
+
+#terraform state list aws_instance.ldap-ubuntu   validation
+
+#terraform state show aws_instance.ldap-ubuntu  show the state of the resource
